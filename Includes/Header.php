77df1e114a4111255c0b47946e811b1042c4a29e
TristandeWit2007 <165791206+TristandeWit2007@users.noreply.github.com>
2024-09-20 14:51:46 +0200

    Begin homepage

---
+++ Includes/Header.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * User: T. de Wit, F. Dada
+ * Date: 20 - 9 - 2024
+ * File: index.php
+ */
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>
+        <?php
+        echo 'HypotheekNavigator';
+        date_default_timezone_set('Europe/Amsterdam');
+        ?>
+    </title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="Styles/Stylesheet.css" rel="stylesheet">
+</head>
+<body>
+<header>
+    <ul>
+        <li>
+            <a href="index.php">Homepage</a>
+        </li>
+        <li>
+            <a href="Pages/OverOns">Over Ons</a>
+        </li>
+        <li>
+            <a href="Pages/VragenLijst">Vragenlijst</a>
+        </li>
+    </ul>
+</header>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    include "Includes/Header.php";
+?>
+<body>
+    <main>
+        <h1>Hypotheek Navigator</h1>
+    </main>
+</body>
+
