ac39010188c573acbbd6ed10e422a50cfe5b833d
TristandeWit2007 <165791206+TristandeWit2007@users.noreply.github.com>
2024-09-20 15:09:12 +0200

    homepage geupdate

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
 <body>
     <main>
         <h1>Hypotheek Navigator</h1>
+        <h2>
+            Welkom bij Hypotheek Navigator
+        </h2>
+        <p>
+            Welkom bij Hypotheek waar wij al uw vragen over het kopen van huizen kunnen beantwoorden.
+        </p>
     </main>
 </body>
 
