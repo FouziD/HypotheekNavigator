51f29105fc3600b3889b0f297c08e3f4f34cb493
TristandeWit2007 <165791206+TristandeWit2007@users.noreply.github.com>
2024-09-26 14:08:53 +0200

    basis tekst

--- index.php
+++ index.php
@@ -8,8 +8,16 @@
             Welkom bij Hypotheek Navigator
         </h2>
         <p>
-            Welkom bij Hypotheek waar wij al uw vragen over het kopen van huizen kunnen beantwoorden.
+            Welkom bij Hypotheek Navigator waar wij al uw vragen over het kopen van huizen kunnen beantwoorden.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elit ipsum, sodales nec metus at, dapibus vestibulum eros. Aenean ullamcorper congue erat nec egestas. In hac habitasse platea dictumst. Quisque consectetur risus vitae malesuada aliquet. Sed aliquam suscipit tempus. Nunc finibus nibh mauris, sed finibus mauris aliquet vel. Vivamus felis eros, finibus quis libero sit amet, mollis viverra ante. Mauris rhoncus metus quis turpis consequat, sit amet tincidunt elit accumsan. Praesent ac ligula vitae nunc lacinia iaculis ac in sapien. Sed ipsum magna, faucibus eget varius non, auctor lacinia nisi. Etiam non sem libero. Sed at facilisis tellus, id ultrices metus. Proin turpis ante, consequat sed tortor ut, mattis malesuada tortor. Suspendisse sed risus magna. Nam viverra condimentum ligula eu suscipit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+        </p>
+        <h2>
+            Lorem ipsum
+        </h2>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elit ipsum, sodales nec metus at, dapibus vestibulum eros. Aenean ullamcorper congue erat nec egestas. In hac habitasse platea dictumst. Quisque consectetur risus vitae malesuada aliquet. Sed aliquam suscipit tempus. Nunc finibus nibh mauris, sed finibus mauris aliquet vel. Vivamus felis eros, finibus quis libero sit amet, mollis viverra ante. Mauris rhoncus metus quis turpis consequat, sit amet tincidunt elit accumsan. Praesent ac ligula vitae nunc lacinia iaculis ac in sapien. Sed ipsum magna, faucibus eget varius non, auctor lacinia nisi. Etiam non sem libero. Sed at facilisis tellus, id ultrices metus. Proin turpis ante, consequat sed tortor ut, mattis malesuada tortor. Suspendisse sed risus magna. Nam viverra condimentum ligula eu suscipit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
         </p>
     </main>
 </body>
-
