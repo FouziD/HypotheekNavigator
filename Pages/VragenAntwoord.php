2c52a27f31d323324c209d4a95be9b850471ff8b
TristandeWit2007 <165791206+TristandeWit2007@users.noreply.github.com>
2024-10-03 14:31:36 +0200

    VragenAntwoord.php en VragenLijst.php gemaakt

--- Pages/VragenAntwoord.php
+++ Pages/VragenAntwoord.php
@@ -4,15 +4,41 @@ include "../Includes/Header.php";
 <body>
 <main>
     <?php
-                    echo "Voer uw voor en achternaam in: " . $_GET["VoorenAchter"] . "<br><br>";
+                    echo "<h4>1. Voer uw voor en achternaam in:</h4> " . $_GET["VoorenAchter"] . "<br><br>";
 
-                    echo "Heeft u al eerdere ervaring met huizen kopen?: " . $_GET["JaofNee"] . "<br><br>";
+                    echo "<h4>2. Heeft u al eerdere ervaring met huizen kopen?:</h4> " . $_GET["JaofNee"] . "<br><br>";
 
-                    echo "Wat is uw leeftijd: " . $_GET["Leeftijd"] . "<br><br>";
+                    echo "<h4>3. Wat is uw leeftijd:</h4> " . $_GET["Leeftijd"] . "<br><br>";
 
-                    echo "Reden voor uitschrijven: " . $_GET["SelectReden"] . "<br><br>";
+                    echo "<h4>4. Wat is uw jaarlijkse bruto inkomen:</h4> " . $_GET["BrutoInkomen"] . "<br><br>";
 
-                    echo "Leerjaar: " . $_GET["LJ"] . "<br><br>";
+                    echo "<h4>5. Heeft u een vast of flexibel arbeidscontract?:</h4> " . $_GET["VastofFlexibel"] . "<br><br>";
+
+                    echo "<h4>6. Hoeveel eigen vermogen kunt u inbrengen?:</h4> " . $_GET["EigenVermogen"] . "<br><br>";
+
+                    echo "<h4>7. Wat is de geschatte waarde van de woning die u wilt kopen?:</h4> " . $_GET["Waarde"] . "<br><br>";
+
+                    echo "<h4>8. Wat is uw huidige woonsituatie?:</h4> " . $_GET["Huidig"] . "<br><br>";
+
+                    echo "<h4>9. Hoeveel personen wonen er momenteel in uw huishouden?:</h4> " . $_GET["Aantal"] . "<br><br>";
+
+                    echo "<h4>10. Bent u van plan om binnen de komende 12 maanden een woning te kopen?:</h4> " . $_GET["Kopen?"] . "<br><br>";
+
+                    echo "<h4>11. Wat is voor u het belangrijkste bij het kopen van een woning?:</h4> " . $_GET["Belangerijkste"] . "<br><br>";
+
+                    echo "<h4>12. Heeft u al eerder een woning gekocht?:</h4> " . $_GET["Eerdergekocht"] . "<br><br>";
+
+                    echo "<h4>13. Hoeveel tijd bent u bereid te besteden aan het zoeken naar een woning?:</h4> " . $_GET["Tijd"] . "<br><br>";
+
+                    echo "<h4>14. Wat is uw favorieten type woning:</h4> " . $_GET["Favo"] . "<br><br>";
+
+                    echo "<h4>15. Welke financieringsvorm overweegt u voor de aankoop van uw woning?:</h4> " . $_GET["FinancieringsVorm"] . "<br><br>";
+
+                    echo "<h4>16. Wat is uw belangrijkste bron van informatie bij het kopen van een woning?:</h4> " . $_GET["Info"] . "<br><br>";
+
+                    echo "<h4>17. Bent u bekend met de huidige huizenmarkt in uw regio?:</h4> " . $_GET["Bekend?"] . "<br><br>";
+
                     ?>
+
 </main>
 </body>
--- Pages/VragenLijst.php
+++ Pages/VragenLijst.php
@@ -11,20 +11,20 @@ include "../Includes/Header.php";
 <main>
     <form action="VragenAntwoord.php" method="get">
         <h4>
-            Voer uw voor en achternaam in<br>
+            1. Voer uw voor en achternaam in<br>
             <input type="text" name="VoorenAchter" >
         </h4>
         <h4>
-            Heeft u al eerdere ervaring met huizen kopen?<br>Ja
+            2. Heeft u al eerdere ervaring met huizen kopen?<br>Ja
             <input type="radio" name="JaofNee" value="Ja" checked>
            Nee <input type="radio" name="JaofNee" value="nee">
         </h4>
         <h4>
-            Wat is uw leeftijd<br>
+            3. Wat is uw leeftijd<br>
             <input type="number" name="Leeftijd" >
         </h4>
         <h4>
-            Wat is uw jaarlijkse bruto inkomen<br>
+            4. Wat is uw jaarlijkse bruto inkomen<br>
             <select name="BrutoInkomen">
                 <option value="Minder dan €30.000">Minder dan €30.000</option>
                 <option value="€30.000 - €50.000">€30.000 - €50.000</option>
@@ -34,7 +34,7 @@ include "../Includes/Header.php";
             </select>
         </h4>
         <h4>
-            Heeft u een vast of flexibel arbeidscontract?<br>
+            5. Heeft u een vast of flexibel arbeidscontract?<br>
             <select name="VastofFlexibel" >
                 <option value="Vast contract">Vast contract</option>
                 <option value="Flexibel contract">Flexibel contract</option>
@@ -43,7 +43,7 @@ include "../Includes/Header.php";
             </select>
         </h4>
         <h4>
-            Hoeveel eigen vermogen kunt u inbrengen?<br>
+            6. Hoeveel eigen vermogen kunt u inbrengen?<br>
             <select name="EigenVermogen">
                 <option value="Minder dan €10.000">Minder dan €10.000</option>
                 <option value="€10.000 - €25.000">€10.000 - €25.000</option>
@@ -53,7 +53,7 @@ include "../Includes/Header.php";
             </select>
         </h4>
         <h4>
-            Wat is de geschatte waarde van de woning die u wilt kopen?<br>
+            7. Wat is de geschatte waarde van de woning die u wilt kopen?<br>
             <select name="Waarde">
                 <option value="Minder dan €200.000">Minder dan €200.000 </option>
                 <option value="€200.000 - €300.000">€200.000 - €300.000 </option>
@@ -63,7 +63,7 @@ include "../Includes/Header.php";
             </select>
         </h4>
         <h4>
-            Wat is uw huidige woonsituatie?<br>
+            8. Wat is uw huidige woonsituatie?<br>
             <select name="Huidig">
                 <option value="Huurwoning">Huurwoning </option>
                 <option value="Koopwoning">Koopwoning </option>
@@ -72,22 +72,26 @@ include "../Includes/Header.php";
             </select>
         </h4>
         <h4>
-            Bent u van plan om binnen de komende 12 maanden een woning te kopen?<br>
+            9. Hoeveel personen wonen er momenteel in uw huishouden?<br>
+            <input type="number" name="Aantal">
+        </h4>
+        <h4>
+            10. Bent u van plan om binnen de komende 12 maanden een woning te kopen?<br>
             Ja,zeker<input type="radio" name="Kopen?" value="Ja,zeker" checked>
             Misschien<input type="radio" name="Kopen?" value="Misschien">
             Nee<input type="radio" name="Kopen?" value="Nee">
         </h4>
         <h4>
-            Wat is voor u het belangrijkste bij het kopen van een woning?<br>
+           11. Wat is voor u het belangrijkste bij het kopen van een woning?<br>
             <input type="text" name="Belangerijkste">
         </h4>
         <h4>
-            Heeft u al eerder een woning gekocht?<br>
+            12. Heeft u al eerder een woning gekocht?<br>
             Ja<input type="radio" name="Eerdergekocht" value="Ja" checked>
             Nee <input type="radio" name="Eerdergekocht" value="nee">
         </h4>
         <h4>
-            Hoeveel tijd bent u bereid te besteden aan het zoeken naar een woning?<br>
+            13. Hoeveel tijd bent u bereid te besteden aan het zoeken naar een woning?<br>
             <select name="Tijd">
                 <option value="Minder dan een maand">Minder dan een maand</option>
                 <option value="1-3 maanden">1-3 maanden</option>
@@ -96,11 +100,11 @@ include "../Includes/Header.php";
             </select>
         </h4>
         <h4>
-            Wat is uw favorieten type woning<br>
+            14. Wat is uw favorieten type woning<br>
             <input type="text" name="Favo">
         </h4>
         <h4>
-            Welke financieringsvorm overweegt u voor de aankoop van uw woning?<br>
+            15. Welke financieringsvorm overweegt u voor de aankoop van uw woning?<br>
             <select name="FinancieringsVorm">
                 <option value="Hypotheek">Hypotheek</option>
                 <option value="Eigen middelen">Eigen middelen</option>
@@ -109,14 +113,14 @@ include "../Includes/Header.php";
             </select>
         </h4>
         <h4>
-            Wat is uw belangrijkste bron van informatie bij het kopen van een woning?<br>
+            16. Wat is uw belangrijkste bron van informatie bij het kopen van een woning?<br>
             <input type="text" name="Info">
         </h4>
         <h4>
-            Bent u bekend met de huidige huizenmarkt in uw regio?<br>
-            ja,zeker<input type="radio" name="Kopen?" value="Ja,zeker" checked>
-            Enigszins<input type="radio" name="Kopen?" value="Enigszins">
-            Nee<input type="radio" name="Kopen?" value="Nee">
+            17. Bent u bekend met de huidige huizenmarkt in uw regio?<br>
+            ja,zeker<input type="radio" name="Bekend?" value="Ja,zeker" checked>
+            Enigszins<input type="radio" name="Bekend?" value="Enigszins">
+            Nee<input type="radio" name="Bekend?" value="Nee">
         </h4>
        <input type="submit">
     </form>
