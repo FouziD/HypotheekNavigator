f9d9defa254830e936f5ada80230cf238d9a070c
FouziD <144001834+FouziD@users.noreply.github.com>
2024-10-03 15:12:24 +0200

    footer include

--- Includes/Footer.php
+++ Includes/Footer.php
@@ -1,8 +1,13 @@
-<?php
+<footer>
+    <p>
+        <?php
+            echo "&copy; Copyright 2024. All rights reserved for Hypotheek Navigtor.";
+        ?>
+    </p>
+
+    <?php
     date_default_timezone_set('europe/amsterdam');
     echo date("h:i") . "<br>";
     echo date("d-m-y");
 ?>
-
-<main>
-</main>
+</footer>
