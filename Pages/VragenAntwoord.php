8847f2ea17198e87f20c613a73da01fc4d794eb5
TristandeWit2007 <165791206+TristandeWit2007@users.noreply.github.com>
2024-10-03 14:04:45 +0200

    VragenAntwoord.php begonnen

--- Pages/VragenAntwoord.php
+++ Pages/VragenAntwoord.php
@@ -1,1 +1,18 @@
 <?php
+include "../Includes/Header.php";
+?>
+<body>
+<main>
+    <?php
+                    echo "Voer uw voor en achternaam in: " . $_GET["VoorenAchter"] . "<br><br>";
+
+                    echo "Heeft u al eerdere ervaring met huizen kopen?: " . $_GET["JaofNee"] . "<br><br>";
+
+                    echo "Wat is uw leeftijd: " . $_GET["Leeftijd"] . "<br><br>";
+
+                    echo "Reden voor uitschrijven: " . $_GET["SelectReden"] . "<br><br>";
+
+                    echo "Leerjaar: " . $_GET["LJ"] . "<br><br>";
+                    ?>
+</main>
+</body>
