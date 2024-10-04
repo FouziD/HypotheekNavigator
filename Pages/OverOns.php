71f1b2bacdeb4441ee235a7e3326c5d69cdbaa8a
FouziD <144001834+FouziD@users.noreply.github.com>
2024-10-04 15:02:41 +0200

    flexbox position

--- Pages/OverOns.php
+++ Pages/OverOns.php
@@ -9,25 +9,30 @@ include "../Includes/Header.php";
 ?>
 <body>
 <main>
+
     <h1>
         Over Ons
     </h1>
     <p>
         Wij zijn de developers van HypotheekNavigator onze namen zijn Tristan de Wit en Fouzi Dada. We zitten beide op het Koning Willem 1 College.
     </p>
-    <h2 class="Flexbox">
-        Tristan de Wit
-    </h2>
-    <img src="../images/" alt="Foto Tristan" class="FlexImg">
-    <p class="Flexbox">
-        Mijn naam is Tristan de Wit en ik ben 17 jaar oud
-    </p>
-    <h2 class="Flexbox">
-        Fouzi Dada
-    </h2>
-    <img src="../images/Fouzi_Dada.jpeg" alt="Foto Fouzi" class="FlexImg">
-    <p class="Flexbox">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur suscipit, dolor congue sodales blandit, sapien massa tempor mauris, consequat tristique orci neque id purus. Fusce eget pretium justo, ac maximus diam. Aliquam erat volutpat. Nunc ultrices ante non purus fringilla tempor. In eget ornare mi. Nunc eu magna diam. Phasellus eu lacus sit amet mauris blandit elementum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis magna suscipit, egestas ex id, ornare lacus. Praesent quis volutpat orci, in iaculis tellus. Nunc facilisis egestas tortor sed feugiat. Donec tempus sed eros id laoreet. Phasellus vel porta ex, in feugiat quam.
-    </p>
+
+    <div id="tristan">
+        <h2>
+            Tristan de Wit
+        </h2>
+        <p>
+            Mijn naam is Tristan de Wit en ik ben 17 jaar oud
+        </p>
+    </div>
+
+    <div id="fouzi">
+        <h2>
+            Fouzi Dada
+        </h2>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur suscipit, dolor congue sodales blandit, sapien massa tempor mauris, consequat tristique orci neque id purus. Fusce eget pretium justo, ac maximus diam. Aliquam erat volutpat. Nunc ultrices ante non purus fringilla tempor. In eget ornare mi. Nunc eu magna diam. Phasellus eu lacus sit amet mauris blandit elementum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis magna suscipit, egestas ex id, ornare lacus. Praesent quis volutpat orci, in iaculis tellus. Nunc facilisis egestas tortor sed feugiat. Donec tempus sed eros id laoreet. Phasellus vel porta ex, in feugiat quam.
+        </p>
+    </div>
 </main>
 </body>
